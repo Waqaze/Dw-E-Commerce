d45060ea67b30b55f0a656eb0a5013002195b2a6
Darshan Kumar <155427044+RDarshankumar@users.noreply.github.com>
2024-08-03 22:52:49 +0500

    GitProduct

--- src/App.js
+++ src/App.js
@@ -1,32 +1,4 @@
 
-// import React from 'react'
-// import Shop from './components/Shop'
-// import Navbar from './components/Navbar'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Contact from './components/Contact'
-// import Home from './components/Home'
-
-// const App =()=>{
-//   return(
-//     <>
-
-// <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/products" element={<Shop />} />
-       
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-//     </Router>
-//    <hr />
-//    <Shop />
-//    <Contact />
-//    </>
-   
-//   )
-// }
-// export default App
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
